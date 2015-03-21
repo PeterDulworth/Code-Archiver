44e46662af877620d09be405ab4019562af1fbff
Peter Dulworth <pdulworth97@gmail.com>
2015-03-20 21:40:02 -0500

    added automatic github pushing

--- codeBackupChron.sh
+++ codeBackupChron.sh
@@ -5,3 +5,9 @@ echo "$currentTime"
 #mkdir $currentTime
 cd /Volumes/BOOTCAMP/Users/Peter/Documents/RobotC/2587z 
 cp -r /Volumes/BOOTCAMP/Users/Peter/Documents/RobotC/2587z/SkyRise  /Users/Peter/Dropbox/Code\ Archive/2587z/$currentTime
+
+cp -r /Volumes/BOOTCAMP/Users/Peter/Documents/RobotC/2587z/SkyRise  /Users/Peter/Documents/Code/GitHub/2587Z/activeArchive/$currentTime
+cd /Users/Peter/Documents/Code/GitHub/2587Z/activeArchive
+git add -A
+git commit -m "$currentTime"
+git push origin master
