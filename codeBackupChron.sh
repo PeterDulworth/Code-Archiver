f51aac9f20b197c84102129d731c66f25fee1260
Peter Dulworth <pdulworth97@gmail.com>
2015-03-20 21:11:03 -0500

    initial commit

---
+++ codeBackupChron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+currentTime=$(date +%Y%m%d%I%M%S)
+echo "$currentTime"
+#cd  /Users/Peter/Dropbox/Code\ Archive
+#mkdir $currentTime
+cd /Volumes/BOOTCAMP/Users/Peter/Documents/RobotC/2587z 
+cp -r /Volumes/BOOTCAMP/Users/Peter/Documents/RobotC/2587z/SkyRise  /Users/Peter/Dropbox/Code\ Archive/2587z/$currentTime
